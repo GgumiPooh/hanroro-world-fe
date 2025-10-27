de2bd145e977964567cd82dd19cb0af8c4113ee6
jeheecheon <jeheecheon@gmail.com>
2025-10-27 23:05:54 +0900

    chore: install react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import Error from "@/routes/Error";
+import Home from "@/routes/Home";
 import "@/styles/globals.css";
 import type { FC } from "react";
+import { Route, Routes } from "react-router";
 
 const App: FC = () => {
-  return <div className="font-medium text-red-500">asd</div>;
+  return (
+    <Routes>
+      <Route errorElement={<Error />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
