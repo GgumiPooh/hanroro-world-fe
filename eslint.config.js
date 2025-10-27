51734e2290503d9a89781160e1416f1b37337583
jeheecheon <jeheecheon@gmail.com>
2025-10-27 22:49:46 +0900

    feat: eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -20,6 +20,22 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: [".*"],
+              message:
+                "Use path imports (@/*) instead of relative imports (../)",
+            },
+          ],
+        },
+      ],
     },
   },
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import "@/styles/globals.css";
 import type { FC } from "react";
-import "./globals.css";
 
 const App: FC = () => {
   return <div className="font-medium text-red-500">asd</div>;
