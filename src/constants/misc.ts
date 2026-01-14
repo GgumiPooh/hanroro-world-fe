376582f0332757d4505a2adfca53c63c0ab6a249
jeheecheon <jeheecheon@gmail.com>
2026-01-14 23:52:41 +0900

    feat: add time constants

---
+++ src/constants/misc.ts
@@ -0,0 +1,7 @@
+export const A_SECOND = 1_000;
+export const A_MINUTE = 60 * A_SECOND;
+export const A_HOUR = 60 * A_MINUTE;
+export const A_DAY = 24 * A_HOUR;
+export const A_WEEK = 7 * A_DAY;
+export const A_MONTH = 30 * A_DAY;
+export const A_YEAR = 365 * A_DAY;
