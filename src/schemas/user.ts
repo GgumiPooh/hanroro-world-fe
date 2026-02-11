a61bde8d43c561008c335bfee613f8189b83896c
jinhyoeun <hyoeun.jin2@gmail.com>
2026-02-11 19:17:38 +0900

    fix: update userSchema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  id: z.number(),
+  id: z.union([z.number(), z.string()]),
   name: z.string(),
-  nickname: z.string(),
-  username: z.string(),
-  email: z.email(),
+  nickname: z.string().optional(),
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  isNewUser: z.boolean().optional(),
 });
